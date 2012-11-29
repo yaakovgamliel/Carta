a48a504a781c3b9a2a5085994a33b5d7f418756f
yasha gamliel <yash0x80@gmail.com>
2012-11-30 01:05:08 +0200

    done with the last challenge!!

--- carta/G2MapPoint.h
+++ carta/G2MapPoint.h
@@ -10,10 +10,13 @@
 #import <CoreLocation/CoreLocation.h>
 #import <MapKit/MapKit.h>
 @interface G2MapPoint : NSObject <MKAnnotation>
-{    
+{
+   // NSDate *dateCreated;
 }
++(id)mapPointTester;
 -(id)initWithCoordinate:(CLLocationCoordinate2D)c title:(NSString *)t;
 @property (nonatomic,readonly) CLLocationCoordinate2D coordinate;
 @property (nonatomic,copy) NSString *title;
+@property (nonatomic,readonly,copy) NSString *subtitle;
 //@property (nonatomic,readonly) NSDate *dateCreated;
 @end
--- carta/G2MapPoint.m
+++ carta/G2MapPoint.m
@@ -9,20 +9,45 @@
 #import "G2MapPoint.h"
 
 @implementation G2MapPoint
+//@synthesize dateCreated;
+@synthesize coordinate;
+@synthesize subtitle;
+//this one work before th @synthe thingi
+@synthesize title;
 -(id)initWithCoordinate:(CLLocationCoordinate2D)c
-                  title:(NSString *)t
+                  title:(NSString *)t 
 {
     self = [super init];
     if (self) {
-        _coordinate = c;//hre we probabily are going to use CLL....Make to
+        coordinate = c;//hre we probabily are going to use CLL....Make to
         [self setTitle:t];   //do it
+        //subtitle = [[[NSDateFormatter alloc]init] stringFromDate:[[NSDate alloc]init]];
+        NSDate *dc = [[NSDate alloc]init];
+        NSDateFormatter *df = [[NSDateFormatter alloc]init];
+        [df setDateStyle:NSDateFormatterMediumStyle];
+        [df setTimeStyle:NSDateFormatterMediumStyle];
+        subtitle = [df stringFromDate:dc];
+        
+        //NSLog(@"%@",subtitle);
     }
     return self;
 }
+//check why we need this
 -(id)init
 {
     return [self initWithCoordinate:CLLocationCoordinate2DMake(43.07, -89.32)
-                                    title:@"Hometown"];
+                                    title:@"Hometown" ];
+}
++(id)mapPointTester
+{
+    G2MapPoint *mapPointerTester = [[G2MapPoint alloc]initWithCoordinate:CLLocationCoordinate2DMake(45.34, -67.90)
+                                                        title:@"Somewhere" ];
+    
+    NSDate *today = [[NSDate alloc]init];
+    NSDateFormatter *dateForm = [[NSDateFormatter alloc]init];
+    NSLog(@"%@",[dateForm stringFromDate:today]);
+    return mapPointerTester;
+    
 }
 
 @end
